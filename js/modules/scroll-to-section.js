29d334e69f116e49511e9bdf2c9970c74ca065dc
Iago Alencar <iagocayman@gmail.com>
2022-08-02 12:54:08 -0300

    add scrollSpy

--- js/modules/scroll-to-section.js
+++ js/modules/scroll-to-section.js
@@ -1,30 +1,50 @@
 // Scroll suave interno
 export default function scrollToSection() {
 
-    const links = document.querySelectorAll('.portfolio-navbar a[href^="#"]');
+    // const links = document.querySelectorAll('.portfolio-navbar a[href^="#"]');
 
-    links.forEach(item => {
-        item.addEventListener('click', scrollToSection);
-    });
+    // links.forEach(item => {
+    //     item.addEventListener('click', scrollToSection);
+    // });
 
-    function scrollToSection(e) {
-        e.preventDefault();
-        const href = e.currentTarget.getAttribute('href');
-        const section = document.querySelector(href);
-        const topo = section.offsetTop;
+    // function scrollToSection(e) {
+    //     e.preventDefault();
+    //     const href = e.currentTarget.getAttribute('href');
+    //     const section = document.querySelector(href);
+    //     const topo = section.offsetTop;
 
-        window.scrollTo({
-            top: topo,
-            behavior: 'smooth',
-        });
-    }
+    //     window.scrollTo({
+    //         top: topo,
+    //         behavior: 'smooth',
+    //     });
+    // }
 
-    for(let i = 0; i < links.length; i++) {
-        links[i].addEventListener('click', () => {
-            for(let j = 0; j < links.length; j++) {
-                links[j].classList.remove('active');
+    // for(let i = 0; i < links.length; i++) {
+    //     links[i].addEventListener('click', () => {
+    //         for(let j = 0; j < links.length; j++) {
+    //             links[j].classList.remove('active');
+    //         }
+    //         links[i].classList.add('active');
+    //     });
+    // }
+
+    // ScrollSpy
+    let section = document.querySelectorAll('section');
+    const navlinks = document.querySelectorAll('.portfolio-navbar a[href^="#"]');
+
+    window.addEventListener('scroll', () => {
+        section.forEach(item => {
+            let top = window.scrollY;
+            let offset = item.offsetTop;
+            let height = item.offsetHeight;
+            let id = item.getAttribute('id');
+
+            if(top >= offset && top < offset + height) {
+                navlinks.forEach(links => {
+                    links.classList.remove('active');
+                    document.querySelector('.portfolio-navbar a[href*=' + id + ']').classList.add('active')
+                });
             }
-            links[i].classList.add('active');
-        })
-    }
+        });
+    });
 }
